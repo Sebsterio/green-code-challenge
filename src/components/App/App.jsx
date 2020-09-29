60bde52e6ca7d384b457b9d33c623dd9992d105f
Sebsterio <sebastian.rosloniec@gmail.com>
2020-09-29 21:49:08 +0200

    preload images in order to fix jittery fadeIn animation on load

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,22 @@
-import React, { Suspense } from "react";
+import React from "react";
 import AuthPage from "components/AuthPage";
+import usePreloadAssets from "hooks/usePreloadAssets";
+
+// to preload
+import Logo from "images/logo-r.svg";
 
 const App = () => {
+	// I wouldn't do this in production due to performance loss
+	// but it improves the impression of the demo
+	const ready = usePreloadAssets({ images: Logo });
+	if (!ready) return null;
+
 	return (
-		<Suspense fallback={null}>
-			<div className="App">
-				<main>
-					<AuthPage />
-				</main>
-			</div>
-		</Suspense>
+		<div className="App">
+			<main>
+				<AuthPage />
+			</main>
+		</div>
 	);
 };
 
---
+++ src/hooks/usePreloadAssets.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+
+// Convert item to array if it isn't one already
+const arrayify = (item) => (Array.isArray(item) ? item : [item]);
+
+// Create an image without attaching it to the DOM
+const preloadImage = ({ src, onload, onerror }) => {
+	const img = new Image();
+	img.onload = onload;
+	img.onerror = onerror;
+	img.src = src;
+};
+
+// load assets in the background and return true when all done
+const usePreloadAssets = (assets) => {
+	const images = arrayify(assets.images);
+	const numAssetsToLoad = images.length;
+	const [numAssetsLoaded, setNumAssetsLoaded] = useState(0);
+
+	const incrementAssetsLoaded = () =>
+		setNumAssetsLoaded((numAssetsLoaded) => numAssetsLoaded + 1);
+
+	const abort = () => setNumAssetsLoaded(numAssetsToLoad);
+
+	useEffect(() => {
+		images.forEach((src) =>
+			preloadImage({
+				src,
+				onload: incrementAssetsLoaded,
+				onerror: abort,
+			})
+		);
+	}, []);
+
+	return numAssetsLoaded === numAssetsToLoad;
+};
+
+export default usePreloadAssets;
